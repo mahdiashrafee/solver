5d8af646806ed0398a47ff69dbabd981c481a336
mahdi <ashrafeem8@gmail.com>
2024-09-18 16:23:48 -0400

    insertionSort fully works

--- sorts/sorts.cpp
+++ sorts/sorts.cpp
@@ -27,13 +27,13 @@ void optionsSorts(int* array){
                 break;
             case '3':
                 printArray(array,7);
-                selectionSort(array, 7);
+                insertionSort(array, 7);
                 printArray(array, 7);
                 break;
             case '4':
                 return;   
-            printSorts();
          }
+         printSorts();
      }
 }
 void swap(int &First, int &Second){
@@ -75,7 +75,13 @@ void insertionSort(int* array, int size){
     std::cout<<"Insertion Sort in Process\n";
     usleep(2000000); //for animation
 
-    for(int i = 0; i<size; i++){
-
+    for(int i = 0; i<size-1; i++){
+        if(array[i]>array[i+1]){
+           for(int j=i; j>=0; j--){
+            if(array[j]>array[j+1])
+                swap(array[j],array[j+1]);
+           }          
+        }          
     }
+    std::cout<<"Sorted ";
 }
